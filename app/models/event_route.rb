459d1b3a1b042fccd14b8da2cee7a813215e6353
Trey Evans <lewis.r.evans@gmail.com>
2014-08-13 12:35:31 -0400

    Added event route.

---
+++ app/models/event_route.rb
@@ -0,0 +1,11 @@
+class EventRoute
+  include ActiveModel::Model
+  attr_accessor :exchange_name, :exchange_kind, :routing_key
+
+  def resolve_exchange(channel)
+    if "default" == self.exchange_kind.to_s.downcase
+      return channel.default_exchange
+    end
+    channel.send(*[self.exchange_kind.to_s, self.exchange_name])
+  end
+end
